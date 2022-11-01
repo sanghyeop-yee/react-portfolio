09e58c91056af203279f087c4f55aa50f8a04410
Sanghyeop Lee <61818855+sanghyeop-yee@users.noreply.github.com>
2022-11-01 22:35:02 +0900

    create Portfolio components jsx, css, responsive design and apply map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
+
+// Project items from Array using map method
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Genie: AI Curated Marketplace for Your Next Gift',
+    github: 'https://github.com/sanghyeop-yee/genie-ecommerce-web',
+    demo: ''
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Genie: E-Commerce Platform Seller Dashboard',
+    github: 'https://github.com/sanghyeop-yee/genie-ecommerce-web',
+    demo: ''
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'GalaBear NFT Minting Website',
+    github: 'https://github.com/sanghyeop-yee/genie-ecommerce-web',
+    demo: ''
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'SaveTheEarth: Fund Raising Website',
+    github: 'https://github.com/sanghyeop-yee/genie-ecommerce-web',
+    demo: ''
+  }
+]
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+              <div className='portfolio__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              </div>
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
