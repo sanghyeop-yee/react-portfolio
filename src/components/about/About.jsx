cd10028555a04f023853987e0e0ac66a80d53626
Sanghyeop Lee <61818855+sanghyeop-yee@users.noreply.github.com>
2022-11-01 16:13:10 +0900

    update About component jsx, css and responsivedesign

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.png' // change to 'me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineDeploymentUnit} from 'react-icons/ai'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='.container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Data Analyst</h5>
+              <small>4+ Years</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Entrepreneur</h5>
+              <small>2+ Years</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineDeploymentUnit className='about__icon'/>
+              <h5>Fullstack Developer</h5>
+              <small>1+ Years</small>
+            </article>
+          </div>
+
+          <p>
+          Hi! My name is Sanghyeop Lee 
+          and I’m a full-stack software engineer
+          with 4+ years helping data-informed decisions.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Grab a Coffee!</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
