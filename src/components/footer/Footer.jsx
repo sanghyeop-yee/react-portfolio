74f7d6d2471e9d0e241caa56581698d17d2f826b
Sanghyeop Lee <61818855+sanghyeop-yee@users.noreply.github.com>
2022-11-02 13:37:23 +0900

    create Footer components jsx, css and responsive designs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {SiNotion} from 'react-icons/si'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>SANGHYEOP</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/john-sanghyeop-lee/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/sanghyeop-yee" target="_blank"><FaGithub/></a>
+        <a href="https://sanghyeop-yee.github.io" target="_blank"><SiNotion/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; SANGHYEOP YEE. Feel Free to Connect and Share.</small>
+      </div>
+    </footer>
   )
 }
 
